506423cfdae1b82f8c7ef5f2a217caff254bf776
Paulo Srur <paulosrur777@gmail.com>
2025-06-18 20:58:05 -0300

    calculadora implementada, falta agregar al programa

--- CalculadoraHistorial/Calc.cs
+++ CalculadoraHistorial/Calc.cs
@@ -9,8 +9,76 @@ namespace EspacioCalculadora
         private bool primeraOperacion = true;
         private List<Operacion> historial = new List<Operacion>();
 
+        public void Sumar(double termino)
+        {
+            historial.Add(new Operacion(dato, termino, TipoOperacion.Suma));
+            dato += termino;
+            primeraOperacion = false;
+        }
 
+        public void Restar(double termino)
+        {
+            historial.Add(new Operacion(dato, termino, TipoOperacion.Resta));
+            dato -= termino;
+            primeraOperacion = false;
+        }
 
+        public void Multiplicar(double termino)
+        {
+            if (primeraOperacion)
+            {
+                dato = termino;
+                primeraOperacion = false;
+            }
+            else
+            {
+                historial.Add(new Operacion(dato, termino, TipoOperacion.Multiplicacion));
+                dato *= termino;
+            }
+        }
 
+        public void Dividir(double termino)
+        {
+            if (termino == 0)
+            {
+                Console.WriteLine("No se puede dividir por cero.");
+                return;
+            }
+
+            if (primeraOperacion)
+            {
+                dato = termino;
+                primeraOperacion = false;
+            }
+            else
+            {
+                historial.Add(new Operacion(dato, termino, TipoOperacion.Division));
+                dato /= termino;
+            }
+        }
+
+        public void Limpiar()
+        {
+            historial.Add(new Operacion(dato, 0, TipoOperacion.Limpiar));
+            dato = 0;
+            primeraOperacion = true;
+        }
+
+        public double Resultado => dato;
+
+        public void MostrarHistorial()
+        {
+            if (historial.Count == 0)
+            {
+                Console.WriteLine("No hay operaciones en el historial.");
+                return;
+            }
+
+            Console.WriteLine("\nHistorial de operaciones:");
+            foreach (var op in historial)
+            {
+                Console.WriteLine(op.ToString());
+            }
+        }
     }
 }
